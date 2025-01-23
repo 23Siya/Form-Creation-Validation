f1426ced74f9a1c6b77bfa3d404e4fc9d9a23bc4
23Siya <156370624+23Siya@users.noreply.github.com>
2025-01-23 16:38:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const form = document.getElementById("registrationForm");
     const feedbackDiv= document.getElementById("form-feedback");
+    
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent form submission
+        const usernameInput = document.getElementById('username').valuex.trim();
+        const emailInput = document.getElementById('email').valuex.trim();
+        const passwordInput = document.getElementById('password').valuex.trim();
+    let isValid=true;
+    const message =[];
+
+    });
+  
 });
