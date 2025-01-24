a05ddd9e063cb99b0d0c967d9458b8c15e8fffe1
23Siya <156370624+23Siya@users.noreply.github.com>
2025-01-24 14:06:50 +0200

    Add files via upload

--- fetch-data.js
+++ fetch-data.js
@@ -11,7 +11,7 @@ try{
 const users = await response.json();
 
 dataContainer.innerHTML = '';
-const userList =document.createElement('li');
+const userList =document.createElement('ul');
 
 users.forEach(user => {
     const listItem = document.createElement('li');
