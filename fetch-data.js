a98d42a3de1ef23df330d7e4093a9bc5eb6dd8c9
23Siya <156370624+23Siya@users.noreply.github.com>
2025-01-24 13:52:20 +0200

    Add files via upload

--- fetch-data.js
+++ fetch-data.js
@@ -1,5 +1,14 @@
 async function fetchUserData() {
     const apiUrl = 'https://jsonplaceholder.typicode.com/users';
     const dataContaner= document.getElementById('api-data');
+try{
+  const response= await fetch(apiUrl);  
+  if(!response.ok)
+{
+    throw new Event(`HTTP error! Status: ${response.status}`);
+}
+
+}
+    
     
 }
