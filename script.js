3eadc15b5b78dec4b2e253d87d1cedf6c25ec2d6
23Siya <156370624+23Siya@users.noreply.github.com>
2025-01-24 11:11:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,36 +3,35 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("registration-form");
     const feedbackDiv= document.getElementById("form-feedback");
     
-    form.addEventListener('submit', (event)=> {
+    form.addEventListener('submit', function(event) {
         event.preventDefault(); // Prevent form submission
-        const username = document.getElementById('username').valuex.trim();
-        const email = document.getElementById('email').valuex.trim();
-        const password = document.getElementById('password').valuex.trim();
+        const username = document.getElementById('username').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const password = document.getElementById('password').value.trim();
     let isValid=true;
     const message =[];
 
      // Validate username
      
-      const usernameError = document.getElementById('usernameError');
-      if (username.value.length <3) {
+      if (username.length <3) {
         isValid=false;
         message.push ("Username must be at least 3 characters long");
       }
 
       // Validate email
 
-        if (!email.include (!email.includes("@") || !email.includes("."))) {
+        if (!email.includes("@") || !email.includes(".")) {
           isValid=false;
           message.push ("email must have @ and . charecters  to be valid");
         }
 
    // Validate password
-   if (password.value.length <8) {
+   if (password.length <8) {
     isValid=false;
     message.push ("password must be at least 8 characters long");
     
    }
-
+   
    feedbackDiv.style.display="block";
    if(isValid){
     feedbackDiv.textContent="Registration Successful";
