0219dfd23d068f2fc29b22b59d93a131b55b0424
23Siya <156370624+23Siya@users.noreply.github.com>
2025-01-24 10:35:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,12 +5,42 @@ document.addEventListener("DOMContentLoaded", () => {
     
     form.addEventListener('submit', function(event) {
         event.preventDefault(); // Prevent form submission
-        const usernameInput = document.getElementById('username').valuex.trim();
-        const emailInput = document.getElementById('email').valuex.trim();
-        const passwordInput = document.getElementById('password').valuex.trim();
+        const username = document.getElementById('username').valuex.trim();
+        const email = document.getElementById('email').valuex.trim();
+        const password = document.getElementById('password').valuex.trim();
     let isValid=true;
     const message =[];
 
+     // Validate username
+     
+      const usernameError = document.getElementById('usernameError');
+      if (username.value) {
+        usernameError.textContent = 'Username must be at least 3 characters long.';
+        return;
+      } else {
+        usernameError.textContent = '';
+      }
+
+      // Validate email
+      
+      const emailError = document.getElementById('emailError');
+      if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(emailInput.value)) {
+        emailError.textContent = 'Please enter a valid email address.';
+        return;
+      } else {
+        emailError.textContent = '';
+      }
+      
+   // Validate password
+      if (password.value.length < 6) {
+        passwordError.textContent = 'Password must be at least 6 characters long.';
+        return;
+      } else {
+        passwordError.textContent = '';
+      }
+      form.submit();
     });
+    
+
   
 });
